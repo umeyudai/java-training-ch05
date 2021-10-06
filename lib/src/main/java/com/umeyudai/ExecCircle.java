7f7b73675bb98d992dbeb8cd33cbdab7c69cc1a3
umeyudai <ume.yudai@gmail.com>
2021-10-06 19:45:00 +0900

    演習問題

--- lib/src/main/java/com/umeyudai/ExecCircle.java
+++ lib/src/main/java/com/umeyudai/ExecCircle.java
@@ -5,6 +5,6 @@ public class ExecCircle {
         Circle c = new Circle("blue", 5.5);
         System.out.println("色　="+c.getColor());
         System.out.println("半径="+c.getRadius());
-        System.out.printf("面積=%6.2%", c.area());
+        System.out.printf("面積=%6.2f", c.area());
     }
 }
